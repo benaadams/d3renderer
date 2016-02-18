bf0ae504d250caa8fa4ffe2fc1b9f3a02cf7e108
Ben Adams <thundercat@illyriad.co.uk>
2016-02-18 06:54:36 +0000

    Update graph.js

--- renderers/graph.js
+++ renderers/graph.js
@@ -95,7 +95,7 @@ function prepareData(data, dataPath) {
 
 var document = require( 'jsdom' ).jsdom();
     
-var margin = {top: 10, right: 20, bottom: 50, left: 40},
+var margin = {top: 10, right: 5, bottom: 50, left: 40},
         width = 888 - margin.left - margin.right,
         height = 200 - margin.top - margin.bottom;
 
